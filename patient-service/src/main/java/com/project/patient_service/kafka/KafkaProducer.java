289b664f864b683f8c801847a3381911b4fb8688
i-amDev <92856531+i-amDev@users.noreply.github.com>
2025-11-14 15:53:50 +0530

    - Created the "KafkaProducer" class and autowired the "KafkaTemplate".

---
+++ patient-service/src/main/java/com/project/patient_service/kafka/KafkaProducer.java
@@ -0,0 +1,14 @@
+package com.project.patient_service.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+
+}
